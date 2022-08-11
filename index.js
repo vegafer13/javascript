70be8dd114b07a71a2b9a3e908fef84b0ab9268d
Jesus Vega <vegafer13@gmail.com>
2022-08-11 13:08:47 -0500

    Actualizacion de texto variables

---
+++ index.js
@@ -0,0 +1,13 @@
+alert('Hola soy un java, desde una archivo externo')
+
+var nombre = 'Chuy',
+    apellido = 'Vega',
+    numero1 = 5,
+    numero2 = 7,
+    resultado = numero1 + numero2,
+    nombreUno = 'Juan';
+
+// document.write(numero1 + numero2)
+//document.write(nombre + ' ' + apellido)
+//document.write(resultado)
+
