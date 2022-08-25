83493bb0348daf0e7ce1150860dde3bb749e9509
Jesus Vega <vegafer13@gmail.com>
2022-08-25 10:26:41 -0500

    Se agrego estructura de html

---
+++ ecommerce-JesusVega/login.js
@@ -0,0 +1,24 @@
+//Login de usuario
+
+
+let user = prompt("Introduzca tu usuario");
+let pass = prompt("Introduzca tu contraseña, tienes 3 intentos");
+let intentos = 2;
+
+while (pass != undefined) {
+    if (intentos > 0) {
+        if (pass === "1234") {
+            alert(`Bienvenido ${user}`);
+            break;
+
+        }else if(pass != "1234") {
+            pass = prompt(`Contraseña invalida, te quedan ${intentos} intentos`);
+            intentos--;
+            continue;
+        }
+        
+    }else{
+        alert("Haz intentado varias veces, intentelo mas tarde");
+        break;
+    }
+}
---
+++ ecommerce-JesusVega/suma.js
@@ -0,0 +1,10 @@
+//Suma de valores
+
+function suma(numero1, numero2) {
+    let resultado = numero1 + numero2;
+    return resultado;   
+}
+
+console.log(suma(5,30));
+
+
